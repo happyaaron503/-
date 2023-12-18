38df584c1019ea4f7428af64b1e5134d925002ce
happyaaron503 <blackknight1251@gmail.com>
2023-12-18 21:02:23 +0800

    細分答錯回答

--- password.py
+++ password.py
@@ -4,11 +4,12 @@ max = 3
 
 while count < max:
     ans = input("請輸入密碼: ")
+    count = count + 1
     if ans == password:
-        print("恭喜答對")
+        print("登入成功")
         break
-    elif ans != password:
-        count = count + 1
+    elif ans != password and max - count != 0:
         print("答錯!剩下", max - count, "次機會")
+    elif ans != password and max - count == 0:
         if max - count == 0:
-            print("帳號已上鎖!!!")
+            print("答錯!帳號已上鎖!!!")
